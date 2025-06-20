e9692d7d2de86b30eb018628e69b8ef6c62e748e
DVelasco9 <d12835571@gmail.com>
2025-06-19 23:03:48 -0700

    .

---
+++ SCRIPTS/menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+var musica_menu = preload("res://GameMusic_ForestTheme_24.mp3")
+
+func _ready() -> void:
+	audio_stream_player.stream = musica_menu
+	audio_stream_player.play()
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://SCENES/nivel_0.tscn")
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://SCENES/options.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
