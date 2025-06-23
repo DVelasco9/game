74df312a83fd7b5b063a961727b7874e491be76b
DVelasco9 <d12835571@gmail.com>
2025-06-22 23:03:14 -0700

    Codigo del personaje casi finalizado

--- SCRIPTS/character_body_2d.gd
+++ SCRIPTS/character_body_2d.gd
@@ -2,48 +2,78 @@ extends CharacterBody2D
 
 @export var move_speed: float
 @export var jump: float
-@export var run: float
+@export var run_speed: float
+
 @onready var animated_sprite = $Sprite2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_facing_right = true
+var running = false
+var ataque_1 = false
+
+func _ready():
+	animated_sprite.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
+	attack_1()
+	estado()
 	ajump(delta)
 	flip()
+	move_x()
 	update_animations()
-	move_x()	
 	move_and_slide()
 
-func update_animations():
-	if not is_on_floor():
-		if velocity.y < 0:
-			animated_sprite.play("jump")
+func move_x():
+	if ataque_1:
+		velocity.x = 0
 		return
-	
-	if velocity.x:
-		animated_sprite.play("walk")
+
+	var input_axis = Input.get_axis("move_left", "move_right")
+
+	if running:
+		velocity.x = input_axis * (move_speed * run_speed)
 	else:
-		animated_sprite.play("idle")
+		velocity.x = input_axis * move_speed
+
+func estado():
+	if Input.is_action_just_pressed("run"):
+		running = true
+	if Input.is_action_just_released("run"):
+		running = false
+
+func ajump(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not ataque_1:
+		velocity.y = -jump
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 func flip():
 	if (is_facing_right and velocity.x < 0) or (not is_facing_right and velocity.x > 0):
 		scale.x *= -1
 		is_facing_right = not is_facing_right
 
-func move_x():
-	var input_axis = Input.get_axis("move_left","move_right")
-	velocity.x = input_axis * move_speed
-	
+func attack_1():
+	if Input.is_action_just_pressed("ataque1") and is_on_floor() and not ataque_1:
+		ataque_1 = true
+		animated_sprite.play("attack_1")
+		velocity.x = 0
 
-func rrun():
-	var input_axis = Input.get_axis("ui_left","ui_right")
-	
-	
+func _on_animation_finished():
+	if animated_sprite.animation == "attack_1":
+		ataque_1 = false
 
-func ajump(delta):
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -400
-	
+func update_animations():
 	if not is_on_floor():
-		velocity.y += gravity * delta	
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		return
+
+	if ataque_1:
+		return  
+
+	if running:
+		animated_sprite.play("run")
+	elif velocity.x != 0:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
